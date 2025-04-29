a97b047d7efa199943883e14ebab852aaccd576b
Bijay Khadka <bk006822@gmail.com>
2025-04-29 14:53:35 +0545

    added:authentication(R/L/Lo)

--- authentication/views.py
+++ authentication/views.py
@@ -6,11 +6,6 @@ from .serializers import RegisterSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-class RegisterView(generics.CreateAPIView):
-    queryset = CustomUser.objects.all()
-    permission_classes = [AllowAny]
-    serializer_class = RegisterSerializer
-
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
@@ -22,3 +17,61 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
+    
+#User Registration View
+ 
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = RegisterSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            return Response({
+                "data": None,
+                "message": "Validation error",
+                "status_code": status.HTTP_400_BAD_REQUEST
+        
+
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+        self.perform_create(serializer)
+        return Response({
+            "data": serializer.data,
+            "message": "User registered successfully",
+            "status_code": status.HTTP_201_CREATED
+        }, status=status.HTTP_201_CREATED
+                        )
+
+# User Login View
+class LoginView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            return Response({
+                "data": None,
+                "message": "Invalid credentials",
+                "status_code": status.HTTP_401_UNAUTHORIZED
+            }, status=status.HTTP_401_UNAUTHORIZED)
+
+        token = serializer.validated_data
+        return Response({
+            "data": token,
+            "message": "Login successful",
+            "status_code": status.HTTP_200_OK
+        }, status=status.HTTP_200_OK)
+        
+# User Logout View
+class LogoutView(generics.GenericAPIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        # Invalidate the token here (if using a blacklist or custom logic)
+        return Response({
+            "data": None,
+            "message": "Logout successful",
+            "status_code": status.HTTP_200_OK
+        }, status=status.HTTP_200_OK)

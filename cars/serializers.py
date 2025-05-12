46089ff5eb7b5f521859b8c74cbfa41c44c4ba41
Bijay Khadka <bk006822@gmail.com>
2025-05-12 12:11:13 +0545

    carserailzier:fixed

--- cars/serializers.py
+++ cars/serializers.py
@@ -6,29 +6,8 @@ class CarSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Car
-        exclude = ['collision_damage_waiver', 'third_party_liability_insurance',
-                   'optional_insurance_add_ons', 'insurance_expiry_date',
-                   'color', 'seats', 'mileage', 'type', 'gearbox', 'max_speed']
-
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation['insurance'] = {
-            'collision_damage_waiver': instance.collision_damage_waiver,
-            'third_party_liability': instance.third_party_liability_insurance,
-            'add_ons': instance.optional_insurance_add_ons,
-            'expiry': instance.insurance_expiry_date,
-        }
-        representation['specs'] = {
-            'color': instance.color,
-            'seats': instance.seats,
-            'mileage': instance.mileage,
-            'vehicle_type': instance.type,
-            'transmission': instance.gearbox,
-            'top_speed': instance.max_speed,
-        }
-        return representation
-
-
+        fields = '__all__'
+        read_only_fields = ['created_at', 'updated_at', 'created_by', 'updated_by', 'deleted_at', 'deleted_by']
 
 class CarListSerializer(serializers.ModelSerializer):
     """Simplified serializer for list view"""

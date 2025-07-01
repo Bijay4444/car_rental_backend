46580989cb17798c7b6487136bc46421fb51c472
Bijay Khadka <bk006822@gmail.com>
2025-07-01 16:41:57 +0545

    Complete Cloudinary integration

--- build.sh
+++ build.sh
@@ -10,3 +10,6 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+# Create superuser if none exists
+python manage.py create_superuser_if_none
